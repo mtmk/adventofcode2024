64c73e0ea5d08e96a409a85299300bcd289c2010
Ziya Suzen <ziya@suzen.net>
2024-12-01 21:31:50 +0000

    2024 day 1

---
+++ day2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+List<string> list1 = new();
+int max = 0;
+
+using var reader = new StreamReader("input");
+while (reader.ReadLine() is { } line)
+{
+    max++;
+    var strings = Regex.Split(line, @"\s+");
+    list1.Add(strings[0]);
+}
+
+foreach (var s in list1)
+{
+    Console.WriteLine(s);
+}
+
+for (int i = 0; i < max; i++)
+{
+    Console.WriteLine(list1[i]);
+}
