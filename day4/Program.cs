405a8ccd3e835e0a122d499106016e2959dfc3d8
Ziya Suzen <ziya@suzen.net>
2024-12-03 14:40:28 +0000

    2024 day 3

---
+++ day4/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+foreach (var line in File.ReadAllLines("input"))
+{
+    var strings = Regex.Split(line, @"\s+");
+
+    foreach (var s in strings)
+    {
+        Console.WriteLine(s);
+    }
+}
